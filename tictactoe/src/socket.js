be9f235c4ba437309d2c83f6a443ad3e73ff28cb
Nivedhan A <49774666+nivedhan-a@users.noreply.github.com>
2024-09-12 18:36:29 +0530

    Update socket.js

--- tictactoe/src/socket.js
+++ tictactoe/src/socket.js
@@ -1,9 +1,23 @@
 // socket.js
+/*import io from 'socket.io-client';
+
+// Create a singleton socket instance
+//const URL = 'http://localhost:3001';
+const URL = 'https://universal-checker-hourglass.glitch.me';
+const socket = io(URL);
+// socket.js
+*/
+
 import io from 'socket.io-client';
 
 // Create a singleton socket instance
 //const URL = 'http://localhost:3001';
-const URL = 'https://tic-tac-toe-multiplayer-api.vercel.app';
-const socket = io(URL);
+const URL = 'https://universal-checker-hourglass.glitch.me';
+const socket = io(URL, {
+  extraHeaders: {
+    "User-Agent": "Custom Client"
+  }
+});
 
+export default socket;
 export default socket;
