f3572b897940c9b74dfbd1b21ada2dd8b358b562
Nivedhan A <49774666+nivedhan-a@users.noreply.github.com>
2024-09-12 08:08:34 +0530

    Update socket.js

--- tictactoe/src/socket.js
+++ tictactoe/src/socket.js
@@ -2,8 +2,8 @@
 import io from 'socket.io-client';
 
 // Create a singleton socket instance
-//const URL = 'http://localhost:3001'; // Adjust as needed
-const URL = 'https://tic-tac-toe-multiplayer-pied.vercel.app'; // Adjust as needed
+//const URL = 'http://localhost:3001';
+const URL = 'https://tic-tac-toe-multiplayer-api.vercel.app';
 const socket = io(URL);
 
 export default socket;
